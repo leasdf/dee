4fc47b1620c09728b1481f3c8c75707b6fe46b53
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-07 01:24:58 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @LegendBoy_XD
+# » @legendDeepanshu 
 
 import os
 import json
@@ -38,7 +38,7 @@ async def button(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.edit_message_text(
             chat_id=update.message.chat.id,
-            text="Buy The Subscriptions From @LegendBoy_XD To Get Access Of Advanced Features Of This Bot",
+            text="Buy The Subscriptions From » @legendDeepanshu  To Get Access Of Advanced Features Of This Bot",
             reply_to_message_id=update.message.message_id,
         )
         return
